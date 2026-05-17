4964b25c013c735c25af3b9decc20b46b61d71f3
kemi-p <kpole@dvtsoftware.com>
2026-05-17 21:27:02 +0200

    fix error in spelling

--- Models/TaskRequests.cs
+++ Models/TaskRequests.cs
@@ -4,4 +4,4 @@ using TaskTracker.TeamTask;
 
 public record CreateTaskRequest(string Title, string? Description, string? AssignedTo, DateTime? DueDate);
 public record AssignRequest(string User);
-public record TransitionRequest(TaskStatus NewStatus);
+public record TransitionRequest(TaskStatuss NewStatus);
